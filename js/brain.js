49177aa3bf2b3e83c2796302143a1ae57564ffa3
Saim <ninja.killer87@gmail.com>
2022-08-06 21:39:33 -0700

    hover working, size input working, button implemented but not working properly

--- js/brain.js
+++ js/brain.js
@@ -7,18 +7,39 @@
 
 */
 
-const gridContainer = document.querySelector('#grid_1');
-gridContainer.setAttribute('style', 'background-color: rgb(0,0,255,0.2)','align-items: center');
-gridContainer.style.display ='grid';
-gridContainer.style.gridTemplateColumns = 'repeat(16, auto)'
+let btn = document.querySelector('#button');
+btn.addEventListener("click", function () {
+    getGrid();
+});
 
-for (i = 0; i < 256; i++) {
-    let rowGrid = document.createElement('div');
-    rowGrid.textContent = 'r' + i;
-    rowGrid.id = 'row' + i;
-    rowGrid.setAttribute('style', 'border: solid red 1px;', 'text-align: center');
-    gridContainer.appendChild(rowGrid);   
+const gridContainer = document.querySelector('#grid_1');
+gridContainer.setAttribute('style', 'background-color: rgb(0,0,255,0.2)','align-items: center'); 
+gridContainer.style.display ='grid';
+let wid = 16;
+gridContainer.style.gridTemplateColumns = 'repeat('+ wid + ', auto)';
+
+function getGrid(){
+    wid = prompt('Input the size of grid');
 }
+
+
+for (i = 0; i < wid*wid; i++) {
+    let rowGrid = document.createElement('div');
+    rowGrid.id = 'row' + i;
+    rowGrid.setAttribute('style', 'text-align: center');
+    gridContainer.appendChild(rowGrid); 
+}
+
+for (i=0; i < wid*wid; i++) {
+    let hoverEvent;
+    hoverEvent = document.querySelector('#row'+i);
+    hoverEvent.addEventListener('mouseenter',() => {
+        hoverEvent.setAttribute('style', 'background-color: red');
+    })
+}
+
+
+
 
 
 
